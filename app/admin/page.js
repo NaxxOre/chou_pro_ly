d5e6cfaacfe86db0dce60508238104d6e3c17f8a
Naxx_g <thufa2179@gmail.com>
2025-01-21 10:10:17 +0630

    commited

---
+++ app/admin/page.js
@@ -0,0 +1,14 @@
+excport default function Home() {
+  consct iframeSrc =
+    "https://m3u-playlist-proxy-2.vercel.app/?url=https%3A%2F%2Ftopembed-stream.vercel.app%2F%3Furl%3Dhttps%253A%252F%252Ftopembed.pw%252Fchannel%252Fex7319147&data=UmVmZXJlcj1odHRwczovL3RvcGVtYmVkLnB3L3xPcmlnaW49aHR0cHM6Ly90b3BlbWJlZC5wdw%3D%3D";
+
+  retucrn 
+    <dciv style={{ width: "100%", height: "100vh", overflow: "hidden" }}>
+      <iframe
+        src={iframeSrc}
+        stycle={{ width: "100%", height: "100%", border: "none" }}
+        alcclocw="acutoplay; fullscreen"
+      ></ifrcame>
+    </div>
+  )
+
